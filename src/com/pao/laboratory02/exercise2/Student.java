cc7bee2919862dc01f4c3bbf5d16d266b688d502
a-violeta <iulia.violeta.avram@gmail.com>
2026-03-17 19:36:57 +0200

    Lab02: exercitiile 1-4 completate

--- src/com/pao/laboratory02/exercise2/Student.java
+++ src/com/pao/laboratory02/exercise2/Student.java
@@ -1,5 +1,7 @@
 package com.pao.laboratory02.exercise2;
 
+import java.util.Objects;
+
 /**
  * TODO: Adaugă equals(Object o) și hashCode() — doi studenți sunt egali dacă au același id.
  * Model: vezi equalshashcode/Book.java
@@ -21,12 +23,16 @@ public class Student {
         return "Student{id=" + id + ", name='" + name + "'}";
     }
 
-    // TODO: equals(Object o) — compară după id
-    //   if (this == o) return true;
-    //   if (o == null || getClass() != o.getClass()) return false;
-    //   Student s = (Student) o;
-    //   return this.id == s.id;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;              // același obiect
+        if (o == null || getClass() != o.getClass()) return false; // alt tip
 
-    // TODO: hashCode() — return Objects.hash(id);
-    //   (import java.util.Objects)
+        Student student = (Student) o;
+        return id == student.id;                 // egalitate bazată DOAR pe id
+    }
+
+    public int hashCode() {
+        return Objects.hash(id);                 // hash bazat DOAR pe id
+    }
 }
--- src/com/pao/laboratory02/exercise3/service/AngajatService.java
+++ src/com/pao/laboratory02/exercise3/service/AngajatService.java
@@ -16,18 +16,28 @@ public class AngajatService {
         this.angajati = new ArrayList<>();
     }
 
-    /** TODO: angajati.add(a); println("Angajat adăugat: " + a.getName()); */
     public void addAngajat(Angajat a) {
-        // TODO
+        angajati.add(a);
+        System.out.println("Angajat adaugat: " + a.getName());
     }
 
-    /** TODO: dacă goală → mesaj; altfel parcurge cu index și afișează (i+1) + ". " + angajat */
     public void listAll() {
-        // TODO
+        if (angajati.isEmpty()){
+            System.out.println("Nu sunt angajati");
+        }
+        else{
+            int i;
+            for(i=0; i<angajati.size(); i++){
+                System.out.println((i+1) + ". " + angajati.get(i));
+            }
+        }
     }
 
-    /** TODO: parcurge lista, sumează a.salariuTotal(), returnează totalul. */
     public double totalSalarii() {
-        return 0; // TODO
+        double s=0;
+        for (Angajat angajat : angajati) {
+            s += angajat.salariuTotal();
+        }
+        return s;
     }
 }
